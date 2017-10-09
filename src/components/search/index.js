07c691c93b57261a3fbfaa9da2ab18395007a13a
Blazh <iblazevi@gmail.com>
2017-10-09 22:36:54 +0200

    Make selectable

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,6 +1,7 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router';
 import style from './style.less';
+import { find } from 'underscore';
 
 export default class Search extends Component {
 
@@ -12,47 +13,92 @@ export default class Search extends Component {
 		this.handleSubmit = this.handleSubmit.bind(this);
 
 		this.filters = [
+			/*
 			{
-				name: "Everything"
+				name: "Everything",
+				selected: true
 			},
 			{
-				name: "New in your area"
+				name: "New in your area",
+				selected: false
 			},
 			{
-				name: "Fashion and Accessories"
+				name: "Fashion and Accessories",
+				selected: false
 			},
 			{
-				name: "Home and Garden"
+				name: "Home and Garden",
+				selected: false
 			},
 			{
-				name: "Electronics"
+				name: "Electronics",
+				selected: false
 			},
 			{
-				name: "Baby and Child"
+				name: "Baby and Child",
+				selected: false
 			},
 			{
-				name: "Sport, Leisure and Games"
+				name: "Sport, Leisure and Games",
+				selected: false
 			},
 			{
-				name: "Movies, Books and Music"
+				name: "Movies, Books and Music",
+				selected: false
 			},
 			{
-				name: "Cars and Motors"
+				name: "Cars and Motors",
+				selected: false
 			},
 			{
-				name: "Property"
+				name: "Property",
+				selected: false
 			},
 			{
-				name: "Services"
-			},
+				name: "Services",
+				selected: false
+			},*/
 			{
-				name: "Other"
+				name: "Other",
+				selected: false,
+				onClick: function(e) {
+
+					this.selected = true;
+
+					e.setState({ selected: true })
+
+
+				},
+				getClass: function() {
+
+					if (this.selected) {
+						return style.filterContainer + ' ' + style.active;
+					} else {
+						return style.filterContainer;
+					}
+
+
+				}
 			}
 		]
 
+		this.filters.onClick = function() {
+			console.log('kaj');
+		}
+
+		console.log(find(this.filters, function(i) { return i.name == "Everything" }));
+
+	}
+
+	onSelectFilter = e => {
+		console.log(e)
+		this.setState({selected: true});
 	}
 
 	handleChange(event) {
+
+
+
 		//console.log(event.target.value)
 		//this.setState({value: event.target.value});
 	}
@@ -63,6 +109,7 @@ export default class Search extends Component {
 	}
 
 	updateText = e => {
+		console.log(this.filters)
 		this.setState({ value: e.target.value });
 	}
 
@@ -82,12 +129,12 @@ export default class Search extends Component {
 								this.filters.map(function(player) {
    									return (
 										<div class={style.sectionContainerItem}>
-											<div class={style.filterContainer}>
+											<div onClick={player.onClick.bind(player, this)} className={player.getClass()}>
 												<i></i><span>{player.name}</span>
 											</div>
 										</div>
 									)
-								})
+								}, this)
 							}
 						</div>
 
